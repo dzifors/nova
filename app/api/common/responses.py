ca74861b60722eb8e042179fff6b28b70698ee79
dzifors <ggrzegrzolka11@gmail.com>
2023-11-17 04:19:09 +0000

    format adjustments

--- app/api/common/responses.py
+++ app/api/common/responses.py
@@ -11,47 +11,37 @@ from typing import TypeVar
 
 from pydantic import BaseModel
 
+
 T = TypeVar("T")
+
 
 class SuccessResponse(BaseModel, Generic[T]):
     status: Literal["success"]
     data: T
     meta: dict[str, Any]
 
+
 def success(
     content: Any,
     status_code: int = 200,
     headers: Optional[dict[str, Any]] = None,
-    meta: Optional[dict[str, Any]] = None
+    meta: Optional[dict[str, Any]] = None,
 ) -> Any:
     if meta is None:
         meta = {}
-    
-    data = {
-        "status": "success",
-        "data": content,
-        "meta": meta
-    }
+
+    data = {"status": "success", "data": content, "meta": meta}
 
     return ORJSONResponse(data, status_code, headers)
+
 
 class ErrorResponse(BaseModel, Generic[T]):
     status: Literal["error"]
     error: T
     message: str
 
-def error(
-    message: str,
-    status_code: int,
-    headers: dict[str, Any] | None = None
-) -> Any:
-    data = {
-        "status": "error",
-        "error": message
-    }
 
-    return ORJSONResponse(
-        content=data,
-        status_code=status_code,
-        headers=headers
-    )
+def error(message: str, status_code: int, headers: dict[str, Any] | None = None) -> Any:
+    data = {"status": "error", "error": message}
+
+    return ORJSONResponse(content=data, status_code=status_code, headers=headers)
