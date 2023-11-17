26df48dd1f24d809018c50e223939e0090e8fc8d
dzifors <ggrzegrzolka11@gmail.com>
2023-11-17 04:26:08 +0000

    new services

--- app/state/services.py
+++ app/state/services.py
@@ -4,6 +4,16 @@ import aiohttp
 import MySQLdb
 import MySQLdb.cursors
 
+import app.settings
+
 http_client: aiohttp.ClientSession
 
-database: MySQLdb.Connection
+_database_connection: MySQLdb.Connection = MySQLdb.connect(
+    host=app.settings.MYSQL_HOST,
+    port=app.settings.MYSQL_PORT,
+    user=app.settings.MYSQL_USERNAME,
+    password=app.settings.MYSQL_PASSWORD,
+    database=app.settings.MYSQL_DATABASE,
+)
+
+database: MySQLdb.cursors.Cursor = _database_connection.cursor()
