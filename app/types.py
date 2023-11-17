d9e56e4aa63a478b8aca8655846aaf27d8e25545
dzifors <ggrzegrzolka11@gmail.com>
2023-11-17 04:12:06 +0000

    formatting changes

--- app/types.py
+++ app/types.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from ipaddress import IPv4Address
-from ipaddress import IPv6Address
+from ipaddress import IPv4Address, IPv6Address
+
 
 IPAddress = IPv4Address | IPv6Address
